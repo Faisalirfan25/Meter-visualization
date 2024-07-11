e03da48c6a1fc300e181f43a9c145f29db6ee7b6
Faisal Irfan <faisalirfan619@gmail.com>
2024-07-11 22:19:39 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
+app.use('/pdfs', express.static(path.join(__dirname, 'public/pdfs')));
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -22,18 +24,23 @@ db.connect(err => {
 
 app.get('/api/:table', (req, res) => {
   const table = req.params.table;
-  const query = `SELECT * FROM ${table}`;
+  const serial_no = req.query.serial_no;
+  let query = `SELECT * FROM ${table}`;
+  
+  if (serial_no) {
+    query += ` WHERE serial_no = ${serial_no}`;
+  }
+  
   db.query(query, (err, results) => {
     if (err) {
       console.error('Error fetching data:', err);
       res.status(500).json({ error: 'Failed to fetch data' });
       return;
     }
-    console.log(results); // Log the results to confirm the data
     res.json(results);
   });
 });
 
-app.listen(5001, () => {
+app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
